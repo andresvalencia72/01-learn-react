247f3f75be10bb0132b5b5f7b7974306c91828a7
Andrés Valencia <andresvalencia7270@gmail.com>
2026-06-23 21:26:06 +0200

    feat: add function and arrow function exercises with TypeScript types

---
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -0,0 +1,43 @@
+function greet(name: string): string {
+    return `Hola ${name}`
+}
+const greet2 = (name: string): string => `Hola ${name}`
+
+const message: string = greet('Vegeta')
+const message2 = greet2('Andrés')
+console.log(message)
+console.log(message2)
+
+
+interface User {
+    uid: string
+    username: string
+    /* addNumber?: ()=> void */
+}
+
+function getUser(): User {
+    return {
+        uid: 'ABC-123',
+        username: 'El_Papi123',
+        /*  age: 24 */
+    }
+}
+
+const getUser2 = (): User => ({
+    uid: 'ABC-123',
+    username: 'El_Papi123',
+})
+
+const user = getUser()
+const user2 = getUser2()
+console.log(user)
+console.log(user2)
+
+
+const myNumbers: number[] = [1, 2, 3, 4, 5]
+
+myNumbers.forEach(function (value) {
+    console.log(value)
+})
+myNumbers.forEach(value => console.log({ value }))
+myNumbers.forEach(console.log)
